2e081d4fcc85b5413db62c484b039c22a23f5073
johnestes4 <johnestes4@gmail.com>
2016-08-19 17:07:13 -0400

    adding new coordinates doesn't totally work yet

--- js/maps/index.controller.js
+++ js/maps/index.controller.js
@@ -10,40 +10,50 @@
     // uiGmapGoogleMapApi is a promise.
     // The "then" callback function provides the google.maps object.
     function getPoints() {
-      return [
-        new google.maps.LatLng(37.782551, -122.445368),
-        new google.maps.LatLng(37.782745, -122.444586),
-        new google.maps.LatLng(37.782842, -122.443688),
-        new google.maps.LatLng(37.782919, -122.442815),
-        new google.maps.LatLng(37.782992, -122.442112),
-        new google.maps.LatLng(37.783100, -122.441461),
-        new google.maps.LatLng(37.783206, -122.440829),
-        new google.maps.LatLng(37.783273, -122.440324),
-        new google.maps.LatLng(37.783316, -122.440023),
-        new google.maps.LatLng(37.783357, -122.439794),
-        new google.maps.LatLng(37.783371, -122.439687)
+      var points = [
+        new google.maps.LatLng(38.902551, -77.035368),
+        new google.maps.LatLng(38.902745, -77.034586),
+        new google.maps.LatLng(38.902842, -77.033688),
+        new google.maps.LatLng(38.902919, -77.032815),
+        new google.maps.LatLng(38.902992, -77.032112),
+        new google.maps.LatLng(38.903100, -77.031461),
+        new google.maps.LatLng(38.903206, -77.030829),
+        new google.maps.LatLng(38.903273, -77.030324),
+        new google.maps.LatLng(38.903316, -77.030023),
+        new google.maps.LatLng(38.903357, -77.029794),
+        new google.maps.LatLng(38.903371, -77.029687)
       ];
+      return points;
     };
 
-    function MockHeatLayer(heatLayer) {
+    function createHeatLayer(heatLayer) {
       var pointArray = new google.maps.MVCArray(getPoints());
       heatLayer.setData(pointArray);
     };
+
+    function updateHeatLayer(heatLayer, newLat, newLong) {
+      var points = getPoints();
+      points.push(new google.maps.LatLng(newLat, newLong));
+      var pointArray = new google.maps.MVCArray(points);
+      console.log(newLat)
+      console.log(newLong)
+      this.heatLayer.setData(pointArray);
+    };
     $scope.map = {
                 center: {
-                latitude: 37.782551,
-                longitude: -122.445368
+                latitude: 38.907240,
+                longitude: -77.036591
                 },
                 showHeat: true,
-                zoom: 12,
+                zoom: 14,
                 heatLayerCallback: function (layer) {
-                  console.log("callback")
                   //set the heat layers backend data
-                  var mockHeatLayer = new MockHeatLayer(layer);
+                  var heatLayer = new createHeatLayer(layer);
                 },
                 toggleHeat: function() {
                   this.showHeat = !this.showHeat;
                 }
+
             };
 
     uiGmapGoogleMapApi.then(function(maps) {
