335e81d52ae5489790ba27f75c12053b1d5199c9
johnestes4 <johnestes4@gmail.com>
2016-08-19 15:55:56 -0400

    heatmaps can now be toggled on and off

--- js/maps/index.controller.js
+++ js/maps/index.controller.js
@@ -9,24 +9,24 @@
     // e.g. $scope.markers = []
     // uiGmapGoogleMapApi is a promise.
     // The "then" callback function provides the google.maps object.
-    function MockHeatLayer(heatLayer) {
-      // Adding 500 Data Points
-      var map, pointarray, heatmap;
-
-      var taxiData = [
-          new google.maps.LatLng(37.782551, -122.445368),
-          new google.maps.LatLng(37.782745, -122.444586),
-          new google.maps.LatLng(37.782842, -122.443688),
-          new google.maps.LatLng(37.782919, -122.442815),
-          new google.maps.LatLng(37.782992, -122.442112),
-          new google.maps.LatLng(37.783100, -122.441461),
-          new google.maps.LatLng(37.783206, -122.440829),
-          new google.maps.LatLng(37.783273, -122.440324),
-          new google.maps.LatLng(37.783316, -122.440023),
-          new google.maps.LatLng(37.783357, -122.439794),
-          new google.maps.LatLng(37.783371, -122.439687)
+    function getPoints() {
+      return [
+        new google.maps.LatLng(37.782551, -122.445368),
+        new google.maps.LatLng(37.782745, -122.444586),
+        new google.maps.LatLng(37.782842, -122.443688),
+        new google.maps.LatLng(37.782919, -122.442815),
+        new google.maps.LatLng(37.782992, -122.442112),
+        new google.maps.LatLng(37.783100, -122.441461),
+        new google.maps.LatLng(37.783206, -122.440829),
+        new google.maps.LatLng(37.783273, -122.440324),
+        new google.maps.LatLng(37.783316, -122.440023),
+        new google.maps.LatLng(37.783357, -122.439794),
+        new google.maps.LatLng(37.783371, -122.439687)
       ];
-      var pointArray = new google.maps.MVCArray(taxiData);
+    };
+
+    function MockHeatLayer(heatLayer) {
+      var pointArray = new google.maps.MVCArray(getPoints());
       heatLayer.setData(pointArray);
     };
     $scope.map = {
@@ -34,12 +34,16 @@
                 latitude: 37.782551,
                 longitude: -122.445368
                 },
+                showHeat: true,
                 zoom: 12,
                 heatLayerCallback: function (layer) {
-                    //set the heat layers backend data
-                    var mockHeatLayer = new MockHeatLayer(layer);
-                    },
-                showHeat: true
+                  console.log("callback")
+                  //set the heat layers backend data
+                  var mockHeatLayer = new MockHeatLayer(layer);
+                },
+                toggleHeat: function() {
+                  this.showHeat = !this.showHeat;
+                }
             };
 
     uiGmapGoogleMapApi.then(function(maps) {
