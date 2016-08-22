86d65202b853f8a7143d60fe8f2f1ac89e4a0033
Christopher Phillips <cphillips918@gmail.com>
2016-08-22 10:01:11 -0400

    updated rake and seed file for land massish data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 $i = 0
-$num = 5
+$num = 500
 while $i < $num  do
-    Map.create(coordinates: [Faker::Address.latitude, Faker::Address.longitude], hashtag:['#'+Faker::Hacker.abbreviation, '#'+Faker::Hipster.word])
-   $i +=1
+    coordinates = [Faker::Address.latitude, Faker::Address.longitude]
+    testLat = coordinates[0].to_f
+    testLong = coordinates[1].to_f
+    if (testLat < 70 && testLat > 30 && testLong < 50 && testLong > 0) || (testLat < 37  && testLat > 0 && testLong < 95 && testLong > 0) || (testLat > -37 && testLat < 0 && testLong > -95 && testLong < 0) || (testLat > -37 && testLat < 0 && testLong > 95 && testLong < 0)
+        Map.create(coordinates: [coordinates[0], coordinates[1]], hashtag:['#'+Faker::Hacker.abbreviation, '#'+Faker::Hipster.word])
+        $i +=1
+    end
 end
+# $$c(W 124°49'00"--W 67°08'00"/N 50°30'00"--N 8°24'00")
+#     if coordinates[0] < 124 && coordinates[0]
