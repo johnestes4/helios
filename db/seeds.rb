d4c3c2729e3a4d97a2e6941ddad74b9a95b045d8
Christopher Phillips <cphillips918@gmail.com>
2016-08-22 10:26:03 -0400

    updated data for NA EURO RUSSIA standardized 6 hashtags see rake files for details. Create insane rake file for db creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,9 @@ while $i < $num  do
     coordinates = [Faker::Address.latitude, Faker::Address.longitude]
     testLat = coordinates[0].to_f
     testLong = coordinates[1].to_f
-    if (testLat < 70 && testLat > 30 && testLong < 50 && testLong > 0) || (testLat < 37  && testLat > 0 && testLong < 95 && testLong > 0) || (testLat > -37 && testLat < 0 && testLong > -95 && testLong < 0) || (testLat > -37 && testLat < 0 && testLong > 95 && testLong < 0)
-        Map.create(coordinates: [coordinates[0], coordinates[1]], hashtag:['#'+Faker::Hacker.abbreviation, '#'+Faker::Hipster.word])
+    tag=['#trump', '#blm', '#isis', '#election2016', '#Im with here', '#cake']
+    if (testLat < 70 && testLat > 30 && testLong < 120 && testLong > 0) || (testLat < 37  && testLat > 0 && testLong < 50 && testLong > 0) || (testLat < 50 && testLat > 30 && testLong > -120 && testLong < -80)
+        Map.create(coordinates: [coordinates[0], coordinates[1]], hashtag:[tag.sample])
         $i +=1
     end
 end
